c52e8eddb4d86fb28b6e2763073c3142b98ca27a
Anjalisen66 <anjalisen8203@gmail.com>
2024-11-30 23:24:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT, REMINDER_BINDING_KEY} = require('./config/serverConfig');
+// const { createChannel } = require('./utils/messageQueue');
+// const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
+
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    
+    app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
+    app.listen(PORT, () => {
+        console.log('Server started at Port:',PORT);
+        
+        // jobs();
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'anjali009sen@gmail.com',
+        //     'This is a testing email',
+        //     'Hey How are you'
+        // );
+    });
+
+}
+
+setupAndStartServer();
